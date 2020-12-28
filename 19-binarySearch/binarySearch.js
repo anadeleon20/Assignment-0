9b65c6e8ccc4e40143f0bdc13b33e2eb1e09bce8
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 23:21:00 -0500

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,10 +1,26 @@
-class MySolution {
-  constructor() {
+class MySolution 
+{
+  constructor() 
+  {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
-    // Insert code here;
+  binarySearch(nums, target) 
+  {
+    let start = 0, end = nums.length-1;
+    while(start <= end)
+    {
+      let mid = Math.floor((start+end)/2);
+      if (nums[mid] == target)
+        
+	    return true;
+      
+      else if (nums[mid] < target)
+         start = mid + 1;
+      else end = mid - 1;
+    }
+    
+    return false;// Insert code here;
   }
 }
 
