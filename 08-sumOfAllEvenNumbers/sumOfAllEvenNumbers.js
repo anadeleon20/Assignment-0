0fe5b956ab00023a5310a51e5d01fc36bda91fa7
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:45:51 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,15 @@
-function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+function sumOfAllEvenNumbers(nums) 
+{
+  
+  let sum = 0; 
+    for(let i=0; i < nums.length;i++)
+    {
+        if(nums[i] % 2 === 0)
+        {
+          sum += nums[i]; 
+        }
+    }
+    return sum; // Insert code here;
 }
 
 // Do not edit this line;
