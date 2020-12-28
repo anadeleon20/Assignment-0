c2116aa47dafa830a8a45481123d2b1fec6cd2ba
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:13:24 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,16 @@
-function titleCaseEdit(title) {
-  // Insert code here;
+function titleCaseEdit(title) 
+{
+  
+  var newTitle = title.split (' ');
+  
+  for(var i=0;i<newTitle.length;i++)
+    {
+      newTitle[i]= newTitle[i].charAt(0).toUpperCase() + newTitle[i].substring(1);
+    }
+   
+  return newTitle.join(' ');
+
+                                     // Insert code here;
 }
 
 // Do not edit this line;
