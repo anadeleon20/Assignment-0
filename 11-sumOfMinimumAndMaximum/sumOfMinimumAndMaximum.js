5d800d94f4abc45c962d75b64ecec136e97b2a5a
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:58:34 -0500

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,24 @@
-function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+function sumOfMinimumAndMaximum(nums)
+{
+  let max = nums[0];
+  let min = nums[0];
+  
+	for(let i = 0; i<nums.length; i++)
+  {
+		if (max < nums[i])
+    {
+			max = nums[i];
+		}
+    
+		if (min > nums[i])
+    {
+		    min = nums[i];
+		}
+	}
+return min + max;
+
+
+// Insert code here;
 }
 
 // Do not edit this line;
