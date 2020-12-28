79772f73c7f73a61c15b384b92a82bbafc2a0d89
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:37:43 -0500

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,17 @@
-function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+function countOfAllBooleansAndStrings(arr) 
+{
+   
+  var count = 0;
+
+  for (var i = 0; i < arr.length; i++)
+  {
+    if (typeof arr[i] == "boolean" || typeof arr[i] == "string" )
+    {
+      count++;
+    }
+  }
+
+  return count;// Insert code here;
 }
 
 // Do not edit this line;
