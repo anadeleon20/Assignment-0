51f6862ec84b62d39691a14921e5153c0e3ffd54
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 23:17:22 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,18 @@
-function pairSum(nums, target) {
-  // Insert code here;
+function pairSum(nums, target)
+{
+  
+  if (nums.length < 2 ) throw "error";
+  
+  let ans = false;
+  for ( let i = 0; i < nums.length; i++)
+  {
+    for ( let j = 0; j < i; j++)
+    {
+      if ((nums[i] + nums[j]) == target ) 
+	    ans = true;
+    }
+  }
+  return ans;// Insert code here;
 }
 
 // Do not edit this line;
