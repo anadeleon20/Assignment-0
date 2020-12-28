384d28f85d55e227e9b25ba37aba7f865d034a7b
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 23:07:27 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,13 @@
-function frequencyCounter(word) {
-  // Insert code here;
+function frequencyCounter(word) 
+{
+  var count = {};
+  word.split('').forEach(function(i) 
+                         {
+    if(count[i]) count[i]++ 
+    else count[i] = 1;
+  });
+  return count;
+// Insert code here;
 }
 
 // Do not edit this line;
