b2fc45f15f7b0bc239f91bae23f09dac204158f7
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 23:02:10 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,27 @@
-function fizzBuzz(start, end) {
-  // Insert code here;
+function fizzBuzz(start, end) 
+{
+  
+  var num = [];
+  for (var i = start; i <= end; i++)
+  {
+    if ( i % 3 == 0 && i % 5 == 0)
+    {
+      num.push("FizzBuzz");
+    }
+    else if ( i % 3 == 0)
+    {
+      num.push("Fizz");
+    }
+    else if ( i % 5 == 0)
+    {
+      num.push("Buzz");
+    }
+    else
+    {
+      num.push(i);
+    }
+  }
+  return num;// Insert code here;
 }
 
 // Do not edit this line;
