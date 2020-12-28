f90563fbe8247ca2fa3f1a8ce70ff1cca5dfa388
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:50:13 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,16 @@
-function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+function sumOfNumsWithinARange(nums, start, end)
+{
+  var count = 0;
+
+  for (var i = 0; i < nums.length; i++)
+  {
+    if (start <= nums[i] && nums[i] <= end)
+    {
+      count++;
+    }
+  }
+  
+  return count;// Insert code here;
 }
 
 // Do not edit this line;
