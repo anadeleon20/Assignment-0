1d74ba800726c6562d16d671a59c20582eddeed2
anadeleon20 <76668510+anadeleon20@users.noreply.github.com>
2020-12-27 22:32:26 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,21 @@
-function countOfAllBooleans(arr) {
-  // Insert code here;
+function countOfAllBooleans(arr) 
+{
+
+   var count = 0;
+   for (var i = 0; i < arr.length; i++)
+   {
+      if (arr[i] === true)
+      {
+      count++;
+      }
+     
+      if (arr[i] === false)
+      {
+      count++;
+      }
+  }
+
+  return count;// Insert code here;
 }
 
 // Do not edit this line;
